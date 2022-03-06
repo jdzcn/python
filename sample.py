b6a4a5f42b36c618affc11aefae3f5465c3e74a5
sb <jdzcn@qq.com>
2022-03-06 18:02:47 +0800

    up

---
+++ sample.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec()

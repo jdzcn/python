cb030ff909007bd930e17766267f14c4b55dd3ad
sb <sb>
2022-07-29 09:48:17 +0800

    product.db update

--- product/mysql_sqlite.py
+++ product/mysql_sqlite.py
@@ -10,10 +10,16 @@ conn=sqlite3.connect('product.db')
 db = conn.cursor()
 print ("数据库打开成功")
 for item in json_array:
-        sql=f"insert into product values('{item['name']}','{item['images']}',{item['cid']},'{item['spec']}',{item['price']})"
+        if '15' in item['tags']:
+                sid=2
+        else:
+                sid=1
+        name=item['name'].replace('景德镇御雅堂手绘','')
+        gid=item['tags'].replace('1,','').split(',')[0]
+        sql=f"insert into product (name,image,cid,sid,gid,spec,price) values('{name}','{item['images']}',{item['cid']},{sid},{gid},'{item['spec']}',{item['price']})"
         print(sql)
-
-	# db.execute(sql)
-
+        db.execute(sql)
 conn.commit()
 conn.close()
+print('Data convert successfully.')
+

51d5b17c6ea0495a3a62c1e98dbd22b79604aafe
sb <jdzcn@qq.com>
2022-07-29 08:55:39 +0800

    mysql_sqlite.py

--- product/mysql_sqlite.py
+++ product/mysql_sqlite.py
@@ -3,31 +3,17 @@ import requests
 import json
 import sqlite3
 
-       	r=requests.get('http://172.96.193.223/product.php')
+r=requests.get('http://172.96.193.223/product.php')
+print(r.status_code)
+json_array = r.json()
+conn=sqlite3.connect('product.db')
+db = conn.cursor()
+print ("数据库打开成功")
+for item in json_array:
+        sql=f"insert into product values('{item['name']}','{item['images']}',{item['cid']},'{item['spec']}',{item['price']})"
+        print(sql)
 
-        print(r.status_code)
+	# db.execute(sql)
 
-        json_array = r.json()
-
-        # for item in json_array:
-        #     store_details = {'id':None,'name':None, 'cid':None,'images':None,'tags':None,'spec':None,'price':None}
-        #     store_details['id'] = item['id']
-        #     store_details['name'] = item['name']
-        #     store_details['cid'] = item['cid']
-        #     store_details['images'] = item['images']
-        #     store_details['tags'] = item['tags']
-        #     store_details['spec'] = item['spec']
-        #     store_details['price'] = item['price']
-
-	conn=sqlite3.connect('product.db')
-
-	db = conn.cursor()
-
-	print ("数据库打开成功")
-
-	for item in json_array:
-		sql='insert into product values('+item['name']+')'
-		db.execute(sql)
-
-	conn.commit()
-	conn.close()
+conn.commit()
+conn.close()
